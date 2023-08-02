a1276f108d38aa1bf6f394977df1339e1d233dc9
ebarbozadev <e.barboza@edu.unipar.br>
2023-08-02 17:59:49 -0300

    Adicionando arquivos

---
+++ Pedido Starbuks/Projeto.dpr
@@ -0,0 +1,14 @@
+program Projeto;
+
+uses
+  Vcl.Forms,
+  frmVCLPrincipal in 'frmVCLPrincipal.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
